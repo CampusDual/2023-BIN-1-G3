616a13d7a5590a3e63f31d1d57a006041e9e5cf2
Martín Carregal Lourido <you@example.com>
2023-08-21 11:49:26 +0200

    detalle de camiones realizado y añadido icono en el menu de la app.

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { ThemeService } from '../shared/theme.service';
 
 @Component({
   selector: 'app-main',
@@ -6,4 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+
+  // public closedSidenavImage: string;
+  // public openedSidenavImage: string;
+  // constructor(protected router: Router, protected themeService: ThemeService) {
+  //   this.checkSidenavImage();
+  //   themeService.onThemeUpdate.subscribe(() => this.checkSidenavImage());
+  // }
+
+  // openAbout() {
+  //   this.router.navigate(["/main/about"]);
+  // }
+
+  // checkSidenavImage() {
+  //   let path = 'assets/images/';
+  //   this.closedSidenavImage = path + (this.themeService.currentTheme.href.indexOf('ontimize-implatform') > -1 ? 'trunks.png' : 'trunks.png');
+  //   this.openedSidenavImage = path + (this.themeService.currentTheme.href.indexOf('ontimize-implatform') > -1 ? 'trunks.png' : 'trunks.png');
+  // }
 }
