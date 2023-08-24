f137f6f92f7869ce353c35640e38b0751e7c9791
Martín Carregal Lourido <you@example.com>
2023-08-24 10:28:13 +0200

    terminada la HU33

--- frontend/src/app/shared/app.menu.config.ts
+++ frontend/src/app/shared/app.menu.config.ts
@@ -1,10 +1,54 @@
-import { MenuRootItem } from 'ontimize-web-ngx';
+import { MenuRootItem } from "ontimize-web-ngx";
 
 export const MENU_CONFIG: MenuRootItem[] = [
   // { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'results', name: 'RESULTS', route: '/main/results', icon: 'view_list' },
-  { id: 'graficas', name: 'GRAPHS', route: '/main/graphs', icon: 'stacked_line_chart' },
-  { id: 'trucks', name: 'TRUCKS', route: '/main/trucks', icon: 'local_shipping' },
-  { id: 'trailers', name: 'TRAILERS', route: '/main/trailers', icon: 'rv_hookup' },
-  { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
+  {
+    id: "Datos",
+    name: "DATOS",
+    icon: "description",
+    opened: true,
+    items: [
+      {
+        id: "results",
+        name: "RESULTS",
+        route: "/main/results",
+        icon: "view_list",
+      },
+      {
+        id: "trucks",
+        name: "TRUCKS",
+        route: "/main/trucks",
+        icon: "local_shipping",
+      },
+      {
+        id: "trailers",
+        name: "TRAILERS",
+        route: "/main/trailers",
+        icon: "rv_hookup",
+      },
+    ],
+  },
+
+  {
+    id: "GRAFICAS",
+    name: "GRAFICAS",
+    icon: "area_chart",
+    opened: false,
+    items: [
+      {
+        id: "graficas",
+        name: "Trucks",
+        route: "/main/graphs",
+        icon: "stacked_line_chart",
+      },
+    ],
+  },
+
+  {
+    id: "logout",
+    name: "LOGOUT",
+    route: "/login",
+    icon: "power_settings_new",
+    confirm: "yes",
+  },
 ];
