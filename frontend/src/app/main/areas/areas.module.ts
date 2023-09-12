dbbaa8eea6277746331c096a5fcda9cc41aa8bb0
Martín Carregal Lourido <you@example.com>
2023-09-12 15:34:03 +0200

    grafica pie chart areas

--- frontend/src/app/main/areas/areas.module.ts
+++ frontend/src/app/main/areas/areas.module.ts
@@ -6,9 +6,16 @@ import { AreasHomeComponent } from "./areas-home/areas-home.component";
 import { AreasDetailComponent } from "./areas-detail/areas-detail.component";
 import { AreasNewComponent } from "./areas-new/areas-new.component";
 import { SharedModule } from "src/app/shared/shared.module";
+import { OChartModule } from "ontimize-web-ngx-charts";
 
 @NgModule({
   declarations: [AreasHomeComponent, AreasDetailComponent, AreasNewComponent],
-  imports: [CommonModule, OntimizeWebModule, AreasRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    OntimizeWebModule,
+    AreasRoutingModule,
+    SharedModule,
+    OChartModule,
+  ],
 })
 export class AreasModule {}
