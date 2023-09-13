3af845a4ba64e6972dc2caab36c4c9d5ffb7a00d
jose.munin <jose.munin@campusdual.com>
2023-09-13 09:11:35 +0200

    fix ancho columnas

--- frontend/src/app/main/areas/areas-home/areas-home.component.ts
+++ frontend/src/app/main/areas/areas-home/areas-home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PieChartConfiguration } from "ontimize-web-ngx-charts";
 
 @Component({
-  selector: 'app-areas-home',
-  templateUrl: './areas-home.component.html',
-  styleUrls: ['./areas-home.component.css']
+  selector: "app-areas-home",
+  templateUrl: "./areas-home.component.html",
+  styleUrls: ["./areas-home.component.css"],
 })
 export class AreasHomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  chartParameters: PieChartConfiguration;
+  constructor() {
+    this.chartParameters = new PieChartConfiguration();
+    this.chartParameters.showLeyend = false;
   }
 
+  ngOnInit() {}
 }
