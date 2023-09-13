4d5f366b50235cb36530ad5bd2a02666774ededb
Martín Carregal Lourido <you@example.com>
2023-09-13 08:52:04 +0200

    alineacion grafica zonas

--- frontend/src/app/main/areas/areas-home/areas-home.component.ts
+++ frontend/src/app/main/areas/areas-home/areas-home.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PieChartConfiguration } from "ontimize-web-ngx-charts";
+import { OTranslateService } from "ontimize-web-ngx";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-areas-home',
-  templateUrl: './areas-home.component.html',
-  styleUrls: ['./areas-home.component.css']
+  selector: "app-areas-home",
+  templateUrl: "./areas-home.component.html",
+  styleUrls: ["./areas-home.component.css"],
 })
 export class AreasHomeComponent implements OnInit {
+  chartParameters: PieChartConfiguration;
+  private translateServiceSubscription: Subscription;
 
-  constructor() { }
+  constructor(private translate: OTranslateService) {
+    this.chartParameters = new PieChartConfiguration();
+    this.chartParameters.cornerRadius = 20;
+    // this.chartParameters.labelType = "value";
+    this.chartParameters.showLeyend = false;
+    this.chartParameters.showTooltip = true;
+    this.chartParameters.noDataMessage = this.translate.get("noDataMessage");
 
-  ngOnInit() {
+    this.translateServiceSubscription = translate.onLanguageChanged.subscribe(
+      () => {
+        this.chartParameters.noDataMessage =
+          this.translate.get("noDataMessage");
+      }
+    );
   }
 
+  ngOnInit() {}
+
+  public ngOnDestroy(): void {
+    this.translateServiceSubscription.unsubscribe();
+  }
 }
